80d9a4142284ab150c8b75b8e8585ded499295d3
DavidCCGITHUB <134532108+DavidCCGITHUB@users.noreply.github.com>
2025-04-30 18:51:38 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,5 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import eslint from "vite-plugin-eslint";
+import eslint from 'vite-plugin-eslint';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), eslint()],
-});
+export default {
+  plugins: [process.env.NODE_ENV !== 'production' && eslint()].filter(Boolean),
+};
