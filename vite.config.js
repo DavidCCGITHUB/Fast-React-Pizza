ea29c2dbb8cf91a29bce8cf0ab8fd6d5416018b7
DavidCCGITHUB <134532108+DavidCCGITHUB@users.noreply.github.com>
2025-04-30 19:10:41 +0100

    Add files via upload
    update vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
-import eslint from 'vite-plugin-eslint';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-export default {
-  plugins: [process.env.NODE_ENV !== 'production' && eslint()].filter(Boolean),
-};
+export default defineConfig({
+  plugins: [react()],
+});
